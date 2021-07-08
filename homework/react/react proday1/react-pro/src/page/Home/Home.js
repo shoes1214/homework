e98ab5d0bae07e8c381bcfa45b1cb921455920b2
丁一 <9381946+ding-yi-hao@user.noreply.gitee.com>
2021-07-08 08:38:20 +0800

    添加home页面功能

--- homework/react/react proday1/react-pro/src/page/Home/Home.js
+++ homework/react/react proday1/react-pro/src/page/Home/Home.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+/* 
+1.若不是登录进去主页的（没有location.state）跳转到/login/code
+*/
 // 结构props的写法
 export default function Home({location}) {
+    if(!location.state){
+        return <Redirect to="/login/code"></Redirect>
+    }
     let src=location.state.avatar
     return (
         <div>
